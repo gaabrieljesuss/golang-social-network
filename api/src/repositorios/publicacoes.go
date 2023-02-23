ae113b415701ed38f3e40ea9cb3e6309184274c7
gaabrieljesuss <gjs8@aluno.ifal.edu.br>
2023-02-22 22:02:10 -0300

    Add publish routes and password update route

---
+++ api/src/repositorios/publicacoes.go
@@ -0,0 +1,161 @@
+package repositorios
+
+import (
+	"api/src/modelos"
+	"database/sql"
+)
+
+type Publicacoes struct {
+	db *sql.DB
+}
+
+func NovoRepositorioDePublicacoes(db *sql.DB) *Publicacoes {
+	return &Publicacoes{db}
+}
+
+func (repositorio Publicacoes) Criar(publicacao modelos.Publicacao) (uint64, error) {
+	statement, erro := repositorio.db.Prepare("INSERT INTO publicacoes (titulo, conteudo, autor_id) values (?, ?, ?)")
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+	
+	resultado, erro := statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacao.AutorID)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
+}
+
+func (repositorio Publicacoes) BuscarPorId(publicacaoID uint64) (modelos.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+		SELECT p.*, u.nick FROM 
+		publicacoes p INNER JOIN usuarios u 
+		ON u.id = p.autor_id WHERE p.id = ?
+	`, publicacaoID)
+
+	if erro != nil {
+		return modelos.Publicacao{}, erro
+	}
+	defer linhas.Close()
+
+	var publicacao modelos.Publicacao
+
+	if linhas.Next() {
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadoEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return modelos.Publicacao{}, erro
+		}
+	}
+
+	return publicacao, nil
+}
+
+func (repositorio Publicacoes) BuscarPublicacoes(usuarioID uint64) ([]modelos.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+	SELECT DISTINCT p.*, u.nick FROM publicacoes p 
+	INNER JOIN usuarios u ON u.id = p.autor_id 
+	INNER JOIN seguidores s ON p.autor_id = s.usuario_id 
+	WHERE u.id = ? or s.seguidor_id = ?
+	ORDER BY 1 DESC`,
+		usuarioID, usuarioID,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var publicacoes []modelos.Publicacao
+
+	for linhas.Next() {
+		var publicacao modelos.Publicacao
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadoEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return nil, erro
+		}
+		publicacoes = append(publicacoes, publicacao)
+	}
+
+	return publicacoes, nil
+}
+
+func (repositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao modelos.Publicacao) error {
+	statement, erro := repositorio.db.Prepare("UPDATE publicacoes set titulo = ?, conteudo = ? WHERE id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+	
+	if _, erro = statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (repositorio Publicacoes) DeletarPublicacao(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare("DELETE FROM publicacoes WHERE id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+	
+	if _, erro = statement.Exec(publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (repositorio Publicacoes) BuscarPorUsuario(usuarioID uint64) ([]modelos.Publicacao, error) {
+	linhas, erro := repositorio.db.Query(`
+	SELECT p.*, u.nick FROM publicacoes p 
+	INNER JOIN usuarios u ON u.id = p.autor_id 
+	WHERE p.autor_id = ?`,
+		usuarioID,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var publicacoes []modelos.Publicacao
+
+	for linhas.Next() {
+		var publicacao modelos.Publicacao
+		if erro = linhas.Scan(
+			&publicacao.ID,
+			&publicacao.Titulo,
+			&publicacao.Conteudo,
+			&publicacao.AutorID,
+			&publicacao.Curtidas,
+			&publicacao.CriadoEm,
+			&publicacao.AutorNick,
+		); erro != nil {
+			return nil, erro
+		}
+		publicacoes = append(publicacoes, publicacao)
+	}
+
+	return publicacoes, nil
+}
